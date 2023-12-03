802bbd94aba93988e3c96606c2384bf171973c45
dentopiasa <castelli_philippe@hotmail.com>
2023-12-03 23:13:06 +0200

    oc

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -4,10 +4,11 @@ import App from './App.jsx'
 import './index.css'
 import Greet from './components/Greet.jsx'
 import Welcome from './components/Welcome.jsx'
+import AnimalApp from './components/AnimalApp.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Greet name="Bruce" heroName="Batman">
+    {/*<Greet name="Bruce" heroName="Batman">
       <p>This is a children prop</p>
     </Greet >
     <Greet name="Clark" heroName="Superman"> 
@@ -16,6 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Greet name="Diana" heroName="Wonder Woman" />
     <Welcome name="Bruce" heroName="Batman" />
     <Welcome name="Clark" heroName="Superman" />
-    <Welcome name="Diana" heroName="Wonder Woman" />
+<Welcome name="Diana" heroName="Wonder Woman" />*/}
+  <AnimalApp />
   </React.StrictMode>,
 )
