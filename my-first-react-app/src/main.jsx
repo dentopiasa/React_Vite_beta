e11660b272923e7b0febf078abf3179ab973605c
dentopiasa <castelli_philippe@hotmail.com>
2023-12-04 20:08:30 +0200

    exercising

--- my-first-react-app/src/main.jsx
+++ my-first-react-app/src/main.jsx
@@ -5,10 +5,15 @@ import './index.css'
 import Greet from './components/Greet.jsx'
 import Welcome from './components/Welcome.jsx'
 import AnimalApp from './components/AnimalApp.jsx'
+import Message from './components/Message.jsx'
+import Counter from './components/Counter.jsx'
+import FunctionClick from './components/FunctionClick.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    {/*<Greet name="Bruce" heroName="Batman">
+    <FunctionClick />
+    {/*<Counter />
+    <Greet name="Bruce" heroName="Batman">
       <p>This is a children prop</p>
     </Greet >
     <Greet name="Clark" heroName="Superman"> 
@@ -17,7 +22,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <Greet name="Diana" heroName="Wonder Woman" />
     <Welcome name="Bruce" heroName="Batman" />
     <Welcome name="Clark" heroName="Superman" />
-<Welcome name="Diana" heroName="Wonder Woman" />*/}
-  <AnimalApp />
+    <Welcome name="Diana" heroName="Wonder Woman" />
+<AnimalApp />*/}
   </React.StrictMode>,
 )
